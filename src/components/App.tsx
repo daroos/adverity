aeb98b101ead7ee9ad13ee1d434dcf3d216cf2a0
Dariusz Osiadacz <dariusz.osiadacz@synerise.com>
2019-11-29 18:46:42 +0100

    Extract unique datasources and campaings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,23 +16,46 @@ interface DataRow {
   Impressions: number;
 }
 
+enum CSVHeaders {
+  DATE = 'Date',
+  DATASOURCE = 'Datasource',
+  CAMPAIGN = 'Campaign',
+  CLICKS = 'Clicks',
+  IMPRESSIONS = 'Impressions',
+}
+
+const addToTempArray = (tempArray: string[], value: string) => {
+  return tempArray.includes(value) ? tempArray : [...tempArray, value];
+};
+
 const App: React.FC = () => {
   const [ loading, setLoading ] = React.useState<boolean>(true);
   const [ data, setData ] = React.useState<DataRow[]>([]);
+  const [ datasources, setDatasources ] = React.useState<string[]>([]);
+  const [ campaigns, setCampaigns ] = React.useState<string[]>([]);
 
   React.useEffect(() => {
     const getData = async () => {
       const { data } = await axios.get(DATA_URL);
 
+      let tempDatasources: string[] = [];
+      let tempCampaigns: string[] = [];
+
       const { data: parsedData } = Papa.parse(data, {
         header: true,
         transform(value: string, field: string | number): any {
           switch (field) {
-            case 'Date':
+            case CSVHeaders.DATE:
               return new Date(value);
-            case 'Clicks':
-            case 'Impressions':
+            case CSVHeaders.CLICKS:
+            case CSVHeaders.IMPRESSIONS:
               return parseInt(value);
+            case CSVHeaders.DATASOURCE:
+              tempDatasources = addToTempArray(tempDatasources, value);
+              return value;
+            case CSVHeaders.CAMPAIGN:
+              tempCampaigns = addToTempArray(tempCampaigns, value);
+              return value;
             default:
               return value;
           }
@@ -40,6 +63,8 @@ const App: React.FC = () => {
       });
 
       setData(parsedData);
+      setDatasources(tempDatasources);
+      setCampaigns(tempCampaigns);
       setLoading(false);
     };
 
